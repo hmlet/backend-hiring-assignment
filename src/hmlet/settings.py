87692b8fe8082e0f90af39b2b7f46b535c25e7f8
ammarkhann <ammar88ammar@gmail.com>
2020-08-06 08:18:25 +0500

    Created Photo Model and added static settings

--- src/hmlet/settings.py
+++ src/hmlet/settings.py
@@ -117,5 +117,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+   
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+
+
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
--- src/photos/models.py
+++ src/photos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+
+
+def upload_location(instance,filename):
+	return 'user-uploaded-photos/%s/%s' %(instance.user.username,filename)
+
+class Photo(models.Model):
+	user        		= models.ForeignKey(User,on_delete=models.CASCADE)
+	image 		 		= models.ImageField(upload_to=upload_location)
+	captions 	   		= models.CharField(max_length=250)
+	is_draft 			= models.BooleanField(default=False)
+	published_date   	= models.DateField(auto_now=False, auto_now_add=True)
+	
+	def __str__(self):
+		return self.captions
